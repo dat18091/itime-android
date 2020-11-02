25bead2013fa805216b31bd748a4a18696c6012f
dat18 <dat18090@gmail.com>
2020-11-02 09:06:14 +0700

    initial file for project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'apps/a1_login/a1_login.dart';
+//import 'package:itime/apps/a12_notification/a12_notification.dart';
+//import 'package:itime/apps/a13_shift_works/a13_shift_works.dart';
+//import 'package:itime/apps/a2_register/a2_signup.dart';
+//import 'package:itime/apps/a3_home/a3_check_in.dart';
+//import 'package:itime/apps/a6_report/a6_report.dart';
+//import 'package:itime/apps/a8_salary/a8_salary.dart';
+//import 'package:itime/apps/a9_reward/a9_reward.dart';
+//import 'package:itime/apps/a7_schedule/a7_schedule.dart';
+//import 'package:itime/blocs/login_bloc.dart';
+//import 'package:provider/provider.dart';
+//
+//import 'apps/a1_login/a1_login.dart';
+//import 'apps/a3_home/a3_home_page.dart';
+//import 'blocs/register_bloc.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,110 +25,33 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return MultiProvider(
+      providers: [
+//        Provider<LoginBloc>(create: (_) => LoginBloc()),
+//        Provider<RegisterBloc>(create: (_) => RegisterBloc()),
+        // Provider<AnotherThing>(create: (_) => AnotherThing()),
+      ],
+      // create: (context) => LoginBloc(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Login(),
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          fontFamily: 'Open Sans',
         ),
+        routes: <String, WidgetBuilder>{
+//          "/home-page": (BuildContext context) => new HomePage(), //ng ban hang
+//          "/log-in": (BuildContext context) => new Login(),
+//          "/sign-up": (BuildContext context) => new Signup(),
+//          "/check-in": (BuildContext context) => new CheckIn(),
+//          "/report": (BuildContext context) => new ReportSummary(),
+//          "/notification": (BuildContext context) => new NotificationManager(),
+//          "/reward": (BuildContext context) => new RewardScreen(),
+//          "/shift-works": (BuildContext context) => new ShiftWorkds(),
+//          "/task_schedule": (BuildContext context) => new TaskSchedule(),
+//          "/salary": (BuildContext context) => new SalarySummary(),
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
